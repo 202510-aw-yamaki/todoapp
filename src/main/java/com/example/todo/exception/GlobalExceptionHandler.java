175173922e0b4125451441915975ca9a7975f7e2
LAPTOP-AIIROU83\academia <202510_aw_yamaki@x-ide.sakura.ne.jp>
2026-02-06 15:26:27 +0900

    NoResourceFoundException を 404 扱いに

--- src/main/java/com/example/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +18,12 @@ public class GlobalExceptionHandler {
         ModelAndView mv = new ModelAndView("error/404");
         mv.addObject("message", ex.getMessage());
         return mv;
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ModelAndView handleStaticNotFound(NoResourceFoundException ex) {
+        logger.warn("Static resource not found", ex);
+        return new ModelAndView("error/404");
     }
 
     @ExceptionHandler(Exception.class)
